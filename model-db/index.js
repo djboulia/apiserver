e862836b2be305b8acac3552dfa9c29ada1869bf
Don Boulia <djboulia@gmail.com>
2022-11-19 15:14:45 -0500

    change returned format to only return attributes vs. entire db record

--- model-db/index.js
+++ model-db/index.js
@@ -16,39 +16,134 @@ const DbModel = function( db, modelName, modelNamePlural ) {
         return (modelNamePlural) ? modelNamePlural : modelName + 's';
     }
     
-    this.create = async function(attributes) {
-        const result = await db.create(modelName, attributes);
+    /**
+     * the backend database stores multiple models in one table, using
+     * the special className field to classify different models.  the
+     * id field uniquely identifies each model in the table.  all user
+     * data for the record is stored in the "attributes" field like this:
+     * {
+     *      id: xxx
+     *      className:
+     *      attributes: {
+     *          modelData1: 'foo', 
+     *          modelData2: 'bar'
+     *      }
+     * }
+     * 
+     * this class implements a single model representing the data
+     * we don't want to expose classNme or attributes, so we simply
+     * return the attributes data and insert the "id" field into it.
+     * so flatten model takes the structure above and converts it to:
+     * 
+     * {
+     *      id: xxx
+     *      modelData1: 'foo',
+     *      modelData2: 'bar'
+     * }
+     * 
+     * @param {*} dbObj db model data to flatten
+     */
+    const flattenModel = function( dbObj ) {
+        const attributes = dbObj.attributes;
+        if (attributes) {
+            attributes.id = dbObj.id;
+        }
+
+        return attributes;
+    }
+
+    /**
+     * for methods that return an array of records, 
+     * flatten each one and return 
+     * 
+     * @param {Array} items an array of db records
+     * @returns an array of flattened db records
+     */
+    const flattenArray = function( items ) {
+        if (!items || items.length === 0) {
+            return [];
+        }
+
+        const result = [];
+
+        for (let i=0; i<items.length; i++) {
+            const item = items[i];
+
+            result.push(flattenModel(item));
+        }
+
         return result;
     }
 
-    this.put = async function(obj) {
+    const copy = function(obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+
+    /**
+     * for the put operation, we need to restore the flattened data
+     * to the format the backend database will recognize, e.g.
+     * 
+     * { 
+     *      id:
+     *      className:
+     *      attributes: {}
+     * }
+     * 
+     * @param {*} obj flattened model data
+     * @returns dbObj
+     */
+    const restoreModel = function( obj ) {
+        const id = obj.id;
+
+        // remove the id field from the object before passing it
+        // to the back end database since the dbObj has its own id field
+        const attributes = copy(obj);
+        attributes.id = undefined;     
+
+        const dbObj = {
+            id: obj.id,
+            className: modelName,
+            attributes: attributes
+        }
+
+        return dbObj;
+    }
+
+    this.create = async function(data) {
+        const obj = restoreModel(data);
+        const result = await db.create(modelName, obj.attributes);
+        return flattenModel(result);
+    }
+
+    this.put = async function(data) {
+        const obj = restoreModel(data);
         const result = await db.put(obj);
-        return result;   
+        return flattenModel(result);
     }
 
     this.findByIds = async function(ids) {
-        const result = await db.findByIds(ids);
-        return result;   
+        const results = await db.findByIds(ids);
+        return flattenArray(results);   
     }
 
     this.findById = async function(id) {
         const result = await db.findById(id);
-        return result;   
+        return flattenModel(result);
     }
 
     this.findByFields = async function(fields) {
-        const result = await db.findByFields(modelName, fields);
-        return result;   
+        const results = await db.findByFields(modelName, fields);
+        return flattenArray(results);   
     }
 
     this.findAll = async function() {
-        const result = await db.findAll(modelName);
-        return result;   
+        const results = await db.findAll(modelName);
+        return flattenArray(results);   
     }
 
     this.deleteById = async function(id) {
         const result = await db.deleteById(id);
-        return result;   
+        return flattenModel(result);
     }
 }
 
